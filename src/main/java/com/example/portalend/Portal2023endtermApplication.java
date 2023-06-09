8dd2be27ba7549680efe7b692f5b192ed72e5668
bellsky <always1sky@gmail.com>
2023-06-09 10:54:34 +0900

    project base

---
+++ src/main/java/com/example/portalend/Portal2023endtermApplication.java
@@ -0,0 +1,13 @@
+package com.example.portalend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portal2023endtermApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Portal2023endtermApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portalend/Portal2023endtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portalend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portal2023endtermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
